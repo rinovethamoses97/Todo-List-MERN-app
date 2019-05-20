495ccc5bd044157f23e1ca954e309e98f06fed32
rino <rinovethamoses97@gmail.com>
2019-05-20 19:28:08 +0530

    added new components to separate concerns :sparkles:

--- client/src/App.js
+++ client/src/App.js
@@ -1,102 +1,12 @@
 import React,{Component} from 'react';
-import axios from "axios";
+import Header from "./components/header";
+import Todo from "./components/Todo";
 class App extends Component{
-  constructor(){
-    super();
-    this.state={content:"",todo:[]};
-    this.componentWillMount=this.componentWillMount.bind(this);
-    this.saveContent=this.saveContent.bind(this);
-    this.handleChange=this.handleChange.bind(this);
-  }
-  componentWillMount(){
-     console.log("Getting the todolist from db");
-     var self=this;
-     axios.post("http://localhost:3001/getContent").then(function(response){
-      if(response.data.status==="success"){
-        var temp=response.data.data;
-        for(var i=0;i<temp.length;i++){
-          temp[i].status=true;
-        }
-        self.setState({todo:response.data.data});
-      }
-      else{
-        alert("Error");
-      }
-     });
-  }
-  saveContent(){
-    console.log(this.state.content);
-    var self=this;
-    axios.post("http://localhost:3001/addContent",{content:this.state.content}).then(function(response){
-        if(response.data.status==="success"){
-          alert("Todo added");
-          var temp=self.state.todo;
-          var newPost=response.data.data;
-          newPost.status=true;
-          temp.push(newPost);
-          self.setState({content:'',todo:temp});
-        }
-        else{
-          alert("Error");
-        }
-    });
-  };
-  handleChange=(event)=>{
-    this.setState({content:event.target.value});
-  }
-  deletePost=(event)=>{
-    var targetId=event.target.id;
-    axios.post("http://localhost:3001/deleteContent",{id:event.target.id}).then((response)=>{
-        if(response.data.status==="success"){
-          console.log("Todo Deleted");
-          var temp=this.state.todo;
-          for(var i=0;i<temp.length;i++){
-            if(temp[i]._id===targetId){
-              temp[i].status=false;
-            }
-          }
-          this.setState({todo:temp});
-        }
-        else{
-          console.log("Error");
-        }
-    })
-  }
   render(){
     return (
       <div className="container">
-        {
-          this.state.todo.map((todo)=>{
-            if(todo.status){
-              return <div className="row" key={todo._id}>
-                    <div className="col-sm-12">
-                      <div className="alert alert-success">
-                        {todo.content}
-                        <span id={todo._id} style={{float:'right',cursor:'pointer'}} onClick={this.deletePost}>X</span>
-                      </div>
-                    </div>
-                  </div>
-            }
-            else{
-              return <div className="row" key={todo._id}>
-                    <div className="col-sm-12">
-                      <div className="alert alert-danger" style={{textDecoration:"line-through"}}>
-                        {todo.content}
-                        {/* <span id={todo._id} style={{float:'right',cursor:'pointer'}} onClick={this.deletePost}>X</span> */}
-                      </div>
-                    </div>
-                  </div>
-            }
-          })
-        }
-        <div className="row">
-          <div className="col-sm-10">
-            <input type="text" className="form-control" value={this.state.content}  placeholder="Enter the ToDo" onChange={this.handleChange}></input>
-          </div>
-          <div className="col-sm-2">
-            <button className="btn btn-primary" onClick={this.saveContent}>Post</button>
-          </div>
-        </div>
+        <Header/>
+        <Todo/>
       </div>
     );
   }
---
+++ client/src/components/header.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+class header extends Component{
+    constructor(){
+        super();
+        this.state={};
+    }
+    render(){
+        return(
+            <h1>Todo List</h1>
+        );
+    }
+}
+export default header;
